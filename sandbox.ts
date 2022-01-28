ae8bfdd2552e0f4bf8552ee161987944b29f892b
Yashwanth Prasaanth Sankar Prasath <syashwanth@ath60532.corp.athenahealth.com>
2022-01-28 12:54:05 +0530

    typescript tutorial

---
+++ sandbox.ts
@@ -0,0 +1,13 @@
+const inputs=document.querySelectorAll('input');
+console.log(inputs);
+for(const input of inputs)
+{
+ 
+    console.log(input);
+}
+let arr:(string|number|boolean)[]=["yash","saju",21];
+arr.push(true);
+for(const temp of arr)
+{
+    console.log(temp);
+}
